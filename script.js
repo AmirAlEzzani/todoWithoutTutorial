b4020d78244f7a5fcd2e012119715b880d20f304
AmirAlEzzani <102375974+AmirAlEzzani@users.noreply.github.com>
2024-03-11 23:40:15 -0700

    cant figure out how to identify each button

--- script.js
+++ script.js
@@ -1,34 +1,33 @@
 let addTask = document.getElementById('addTask');
 let taskList = document.getElementById('taskList');
 let setTaskName = document.getElementById('setTaskName');
-let task0 = document.getElementById('task0');
-let taskName0 = document.getElementById('taskName0');
-let rename0 = document.getElementById('rename0');
-let del0 = document.getElementById('del0');
 let renameInput0 = document.getElementById('renameInput0');
 let renameInputBtn0 = document.getElementById('renameInputBtn0');
-
-/* add task */
+let i = 0;
+let renames = [];
+let deletes = [];
+/* add task and btns */
 addTask.addEventListener("click", function() {
-
-
-    var li = document.createElement('li');
-    li.appendChild(document.createTextNode(setTaskName.value));
-    var hi = li.appendChild(document.createElement('h1'));
-    hi.innerText('hello');
-    li = li+hi;
-    taskList.appendChild(li);
-
-
-
-
-
+    let task = document.createElement('li');
+    const rename = document.createElement('button');
+    const del = document.createElement('button');
+    task.appendChild(document.createTextNode(setTaskName.value));
+    taskList.appendChild(task);
+    task.appendChild(rename);
+    task.appendChild(del);
+    rename.innerHTML = "Rename";
+    del.innerHTML = "Delete";
+    rename.setAttribute('id',i);
+    rename.setAttribute('class','renameBtn')
+    let renameBtn = document.getElementsByClassName('renameBtn');
+    del.setAttribute('id',i);
+    i++;  
 })
 
 /* rename task */
-renameInputBtn.addEventListener("click", function() {
-
-})
+    renameBtn.addEventListener("click", function() {
+        /* get the id of the rename btn corresponding to the
+    })
 
 /* delete task */
-del0.addEventListener
+
