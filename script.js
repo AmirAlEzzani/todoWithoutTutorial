b733fca868ce529181e0b0cb9e85c798f4466b73
AmirAlEzzani <102375974+AmirAlEzzani@users.noreply.github.com>
2024-03-13 23:18:52 -0700

    failed

--- script.js
+++ script.js
@@ -1,45 +1,62 @@
-let addTask = document.getElementById('addTask');
-let taskList = document.getElementById('taskList');
-let setTaskName = document.getElementById('setTaskName');
-let renameInput0 = document.getElementById('renameInput0');
-let renameInputBtn0 = document.getElementById('renameInputBtn0');
-let i = 0;
-let renames = [];
-let deletes = [];
-/* add task and btns */
-addTask.addEventListener("click", function() {
-    let task = document.createElement('li');
-    const rename = document.createElement('button');
-    const del = document.createElement('button');
-    task.appendChild(document.createTextNode(setTaskName.value));
-    taskList.appendChild(task);
-    task.appendChild(rename);
-    task.appendChild(del);
-    rename.innerHTML = "Rename";
-    del.innerHTML = "Delete";
-    rename.setAttribute('class','renameBtn')
-    rename.setAttribute('id',i);
-    task.setAttribute('id',i);
-    del.setAttribute('id',i);
-    i++;  
-})
+window.addEventListener('load', () => {
+	const form = document.querySelector("#new-task-form");
+	const input = document.querySelector("#new-task-input");
+	const list_el = document.querySelector("#tasks");
 
-/* rename task */
-var renameButton = document.getElementsByClassName('renameBtn');
+	form.addEventListener('submit', (e) => {
+		e.preventDefault();
 
-PSEUDO CODE BELOW
-document.querySelector('.renameBtn').addEventListener("click", function() {
-    let change = this.id;
-    
-    rename.classList.add('hidden')
-    del.classList.add('hidden')
-    task.appendChild(renameInput);
-    task.appendChild(renameInputBtn);
-    renameInputBtn.addEventListener("click", function(){
-        task.innerText = renameInput.value;
-    })
-    
-})
+		const task = input.value;
 
-/* delete task */
-``
+		const task_el = document.createElement('div');
+		task_el.classList.add('task');
+
+		const task_content_el = document.createElement('div');
+		task_content_el.classList.add('content');
+
+		task_el.appendChild(task_content_el);
+
+		const task_input_el = document.createElement('input');
+		task_input_el.classList.add('text');
+		task_input_el.type = 'text';
+		task_input_el.value = task;
+		task_input_el.setAttribute('readonly', 'readonly');
+
+		task_content_el.appendChild(task_input_el);
+
+		const task_actions_el = document.createElement('div');
+		task_actions_el.classList.add('actions');
+		
+		const task_edit_el = document.createElement('button');
+		task_edit_el.classList.add('edit');
+		task_edit_el.innerText = 'Edit';
+
+		const task_delete_el = document.createElement('button');
+		task_delete_el.classList.add('delete');
+		task_delete_el.innerText = 'Delete';
+
+		task_actions_el.appendChild(task_edit_el);
+		task_actions_el.appendChild(task_delete_el);
+
+		task_el.appendChild(task_actions_el);
+
+		list_el.appendChild(task_el);
+
+		input.value = '';
+
+		task_edit_el.addEventListener('click', (e) => {
+			if (task_edit_el.innerText.toLowerCase() == "edit") {
+				task_edit_el.innerText = "Save";
+				task_input_el.removeAttribute("readonly");
+				task_input_el.focus();
+			} else {
+				task_edit_el.innerText = "Edit";
+				task_input_el.setAttribute("readonly", "readonly");
+			}
+		});
+
+		task_delete_el.addEventListener('click', (e) => {
+			list_el.removeChild(task_el);
+		});
+	});
+});
